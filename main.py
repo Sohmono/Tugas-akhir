c27bfb4d61cb5b945d2d73fefe305968e4a0f329
123_Sohmono Sodip Manik <114635898+Sohmono@users.noreply.github.com>
2025-06-12 05:38:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def main_page():
 
     st.markdown(f"""
         <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
-            <iframe src="https://www.youtube.com/live/Wj-t1-leK2c"
+            <iframe src="https://www.youtube.com/embed/Wj-t1-leK2c"
                     style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
